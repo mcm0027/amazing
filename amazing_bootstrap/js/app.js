e65ba0797bb077b12ca502a14a4363f6f9aaed98
Matt Morris <matthew.cameron.morris@gmail.com>
2015-09-23 17:10:30 -0500

    gallery and lightbox added

--- amazing_bootstrap/js/app.js
+++ amazing_bootstrap/js/app.js
@@ -1,32 +1,32 @@
-var $overlay = $('<div id="overlay"></div>');
+//var $overlay = $('<div id="overlay"></div>');
 
-var $image = $("<img>");
+//var $image = $("<img>");
 
-var $caption = $("<p></p>");
+//var $caption = $("<p></p>");
 
- $overlay.append($image);
- $overlay.append($caption);
- $("body").append($overlay);
+ //$overlay.append($image);
+ //$overlay.append($caption);
+ //$("body").append($overlay);
 
-$("#gallery a").click(function(event) {
-    event.preventDefault();
-    var imageLocation = $(this).attr("href");
+//$("#gallery a").click(function(event) {
+ //   event.preventDefault();
+ //   var imageLocation = $(this).attr("href");
 
 
-    $image.attr("src", imageLocation);
-   	$overlay.show();
-   	var captionText = $(this).children("img").attr("alt");
-   	$caption.text(captionText);
+  //  $image.attr("src", imageLocation);
+ //  	$overlay.show();
+  // 	var captionText = $(this).children("img").attr("alt");
+ // 	$caption.text(captionText);
   
   
 
 
 
-});
+//});
 
- $overlay.click(function() {
-    $(this).hide();
-    });
+ //$overlay.click(function() {
+  //  $(this).hide();
+  //  });
 
 /// Home page stylings
 
