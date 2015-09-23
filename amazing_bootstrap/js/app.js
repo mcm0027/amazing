b6834086ed1a8a3d344e18e9e3071874e106cc96
Matt Morris <matthew.cameron.morris@gmail.com>
2015-09-23 09:26:12 -0500

    added gallery and contact page to bootstrap

--- amazing_bootstrap/js/app.js
+++ amazing_bootstrap/js/app.js
@@ -35,16 +35,16 @@ $("#gallery a").click(function(event) {
 
 //$("#frontFoot").hide().show(2000);
 
-$(document).ready(function() {
+//$(document).ready(function() {
 
-   var docHeight = $(window).height();
-   var footerHeight = $('#footer').height();
-   var footerTop = $('#footer').position().top + footerHeight;
+  // var docHeight = $(window).height();
+ //  var footerHeight = $('#footer').height();
+ //  var footerTop = $('#footer').position().top + footerHeight;
 
-   if (footerTop < docHeight) {
-    $('#footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
-   }
-  });
+//   if (footerTop < docHeight) {
+  //  $('#footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+ //  }
+  //});
 
 
 $(".animsition").animsition({
